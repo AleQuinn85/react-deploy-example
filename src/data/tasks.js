fe13cc7d50c2dad1bc030c1e33ecd66e8dcb9835
AleQuinn85 <alejandroquinn@outlook.com>
2023-03-12 17:30:45 -0600

    first commit

---
+++ src/data/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+    {
+        id : 0,
+        title : 'Mi Primer Tarea',
+        descripcion : 'Mi Primer Tarea',
+    },
+    {
+        id : 1,
+        title : 'Mi Segunda Tarea',
+        descripcion : 'Mi Segunda Tarea',
+    },
+    {
+        id : 2,
+        title : 'Mi Tercer Tarea',
+        descripcion : 'Mi Tercer Tarea',
+    },
+]
